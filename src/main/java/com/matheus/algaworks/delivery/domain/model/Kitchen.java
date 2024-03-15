3a6e38e324e3d63c5cab9ab023f65ab54aaf6bae
Matheus Bespalec <matheusbespalec@gmail.com>
2024-03-14 21:02:18 -0300

    feat: customizar representação de objetos

--- src/main/java/com/matheus/algaworks/delivery/domain/model/Kitchen.java
+++ src/main/java/com/matheus/algaworks/delivery/domain/model/Kitchen.java
@@ -1,25 +1,27 @@
 package com.matheus.algaworks.delivery.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
+@JsonRootName("culinaria")
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Kitchen {
     @Id
+    @JsonIgnore
     @EqualsAndHashCode.Include
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @JsonProperty("nome")
     private String name;
-
-    public Kitchen () { }
-
-    public Kitchen (String name) {
-        this.name = name;
-    }
 }
