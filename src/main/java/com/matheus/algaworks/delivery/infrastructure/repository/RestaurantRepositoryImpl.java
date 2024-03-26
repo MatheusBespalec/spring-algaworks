f9384409a5eda0f9668dce2f0e2d70bc3b1afa74
Matheus Bespalec <matheusbespalec@gmail.com>
2024-03-25 21:38:34 -0300

    feat: consulta customizada com criteria api

--- src/main/java/com/matheus/algaworks/delivery/infrastructure/repository/RestaurantRepositoryImpl.java
+++ src/main/java/com/matheus/algaworks/delivery/infrastructure/repository/RestaurantRepositoryImpl.java
@@ -5,11 +5,15 @@ import com.matheus.algaworks.delivery.domain.repository.RestaurantRepositoryQuer
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -18,25 +22,28 @@ public class RestaurantRepositoryImpl implements RestaurantRepositoryQueries {
     private EntityManager entityManager;
     @Override
     public List<Restaurant> customQuery(String name, BigDecimal minFreightRate, BigDecimal maxFreightRate) {
-        StringBuilder jpql = new StringBuilder("from Restaurant where 1 = 1 ");
-        HashMap<String, Object> paramters = new HashMap<String, Object>();
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Restaurant> criteriaQuery = criteriaBuilder.createQuery(Restaurant.class);
+
+        Root<Restaurant> root = criteriaQuery.from(Restaurant.class);
+
+        ArrayList<Predicate> predicates = new ArrayList<Predicate>();
+
         if (StringUtils.hasLength(name)) {
-            jpql.append("and name like :name ");
-            paramters.put("name", "%" + name + "%");
+            predicates.add(criteriaBuilder.like(root.get("name"), "%" + name + "%"));
         }
 
         if (minFreightRate != null) {
-            jpql.append("and freightRate >= :minFreightRate ");
-            paramters.put("minFreightRate", minFreightRate);
+            predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get("freightRate"), minFreightRate));
         }
 
         if (maxFreightRate != null) {
-            jpql.append("and freightRate <= :maxFreightRate ");
-            paramters.put("maxFreightRate", maxFreightRate);
+            predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get("freightRate"), maxFreightRate));
         }
 
-        TypedQuery<Restaurant> query = this.entityManager.createQuery(jpql.toString(), Restaurant.class);
-        paramters.forEach(query::setParameter);
+        criteriaQuery.where(predicates.toArray(new Predicate[0]));
+
+        TypedQuery<Restaurant> query = this.entityManager.createQuery(criteriaQuery);
         return query.getResultList();
     }
 }
